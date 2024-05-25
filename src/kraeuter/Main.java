1ec078671bdaa564f05195bce34fd1c46880cf2c
Kore1216 <dominicwondra121@gmail.com>
2024-05-25 14:16:05 +0200

    agfcv

--- src/kraeuter/Main.java
+++ src/kraeuter/Main.java
@@ -3,6 +3,6 @@ package kraeuter;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World. Ich liebe Riggelz!");
-		//Kleiner Test.
+		//Kleiner Test.fsdf
 	}
 }
